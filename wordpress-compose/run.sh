afa0d2c080d9bb36c8889517289996ea739d09f2
AKHIL PRADEEP <akhilp1708@gmail.com>
2021-07-05 14:14:47 +0530

    Update run.sh

--- wordpress-compose/run.sh
+++ wordpress-compose/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker-compose up -d
+echo ""
+echo "If you are using Docker Machine, you can run the command docker-machine ip MACHINE_VM to get the machine address, and then open http://MACHINE_VM_IP:8000 in a web browser."
